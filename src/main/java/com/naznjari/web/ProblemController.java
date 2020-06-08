524a33e4fa53e57fec96b337be95691dd4f8d888
nine-hundred <ninehundred@outlook.kr>
2020-06-08 21:44:30 +0900

    회원 가입 화면
    header에서 필요없는 부분 제거

--- src/main/java/com/naznjari/web/ProblemController.java
+++ src/main/java/com/naznjari/web/ProblemController.java
@@ -5,8 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProblemController {
-    @GetMapping()
-    public String problem() {
-        return "problem";
-    }
+
 }
