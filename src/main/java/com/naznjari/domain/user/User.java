c38bbbd121b7f0b7e715362bf999a01391028dae
nine-hundred <ninehundred@outlook.kr>
2020-06-09 20:10:10 +0900

    Google 연동 로직

---
+++ src/main/java/com/naznjari/domain/user/User.java
@@ -0,0 +1,47 @@
+package com.naznjari.domain.user;
+
+import com.naznjari.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
